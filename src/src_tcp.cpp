9061c94023a52c6d12b3e7c24a17d5f73d6f45c9
MatchaOnMuffins <henryzhangjcle@gmail.com>
2022-11-13 23:15:00 -0500

    Code formatting

--- src/src_tcp.cpp
+++ src/src_tcp.cpp
@@ -1,95 +1,85 @@
 #include "src_tcp.h"
 
-SolarTcpPacket *
-createPacket (uint8_t length, char *data, PACKET_TYPE packetType)
+SolarTcpPacket *createPacket(uint8_t length, char *data, PACKET_TYPE packetType)
 {
-    auto *packet = (SolarTcpPacket *)malloc (sizeof (SolarTcpPacket) + length);
-    if (packet == nullptr)
-        {
-            perror ("malloc");
-        }
-    // using malloc instead of new because we need to use free later
-    packet->type = packetType;
-    packet->length = length;
-    memcpy (packet->data, data, length);
-    return packet;
+	auto *packet =
+		(SolarTcpPacket *)malloc(sizeof(SolarTcpPacket) + length);
+	if (packet == nullptr) {
+		perror("malloc");
+	}
+	// using malloc instead of new because we need to use free later
+	packet->type = packetType;
+	packet->length = length;
+	memcpy(packet->data, data, length);
+	return packet;
 }
 
-void
-string2hexString (char *input, char *output)
+void string2hexString(char *input, char *output)
 {
-    int loop;
-    int i;
+	int loop;
+	int i;
 
-    i = 0;
-    loop = 0;
+	i = 0;
+	loop = 0;
 
-    while (input[loop] != '\0')
-        {
-            sprintf ((char *)(output + i), "%02X", input[loop]);
-            loop += 1;
-            i += 2;
-        }
+	while (input[loop] != '\0') {
+		sprintf((char *)(output + i), "%02X", input[loop]);
+		loop += 1;
+		i += 2;
+	}
 }
 
-SolarTcpPacketHeader *
-createPacketHeader (DEVICES devices)
+SolarTcpPacketHeader *createPacketHeader(DEVICES devices)
 {
-    if (devices == SERVER)
-        {
-            auto *header = new SolarTcpPacketHeader{};
-            header->sender = SERVER;
-            header->receiver = CLIENT;
-            return header;
-        }
-    if (devices == CLIENT)
-        {
-            auto *header = new SolarTcpPacketHeader{};
-            header->sender = CLIENT;
-            header->receiver = SERVER;
-            return header;
-        }
-    return nullptr;
+	if (devices == SERVER) {
+		auto *header = new SolarTcpPacketHeader{};
+		header->sender = SERVER;
+		header->receiver = CLIENT;
+		return header;
+	}
+	if (devices == CLIENT) {
+		auto *header = new SolarTcpPacketHeader{};
+		header->sender = CLIENT;
+		header->receiver = SERVER;
+		return header;
+	}
+	return nullptr;
 }
 
-SolarTcp *
-createSolarTcp (SolarTcpPacketHeader *header, SolarTcpPacket *packet)
+SolarTcp *createSolarTcp(SolarTcpPacketHeader *header, SolarTcpPacket *packet)
 {
-    auto *solarTcp = new SolarTcp{ header, packet };
-    return solarTcp;
+	auto *solarTcp = new SolarTcp{ header, packet };
+	return solarTcp;
 }
 
-char *
-SolarTcpRaw (SolarTcp *packet)
+char *SolarTcpRaw(SolarTcp *packet)
 {
-    // convert char * to hex string
-    char *dataHex = (char *)malloc (packet->packet->length * 2);
-    string2hexString (packet->packet->data, dataHex);
-    int packetLength = packet->packet->length * 2;
-    char *buffer = new char[32 + packetLength + 8];
-    sprintf (buffer, "%08x", packet->header->timestamp);
-    sprintf (buffer + 8, "%04x", packet->header->sequence);
-    sprintf (buffer + 12, "%04x", packet->header->sender);
-    sprintf (buffer + 16, "%04x", packet->header->receiver);
-    sprintf (buffer + 20, "%02x", packet->packet->type);
-    // padding
-    sprintf (buffer + 22, "%02x", 0);
+	// convert char * to hex string
+	char *dataHex = (char *)malloc(packet->packet->length * 2);
+	string2hexString(packet->packet->data, dataHex);
+	int packetLength = packet->packet->length * 2;
+	char *buffer = new char[32 + packetLength + 8];
+	sprintf(buffer, "%08x", packet->header->timestamp);
+	sprintf(buffer + 8, "%04x", packet->header->sequence);
+	sprintf(buffer + 12, "%04x", packet->header->sender);
+	sprintf(buffer + 16, "%04x", packet->header->receiver);
+	sprintf(buffer + 20, "%02x", packet->packet->type);
+	// padding
+	sprintf(buffer + 22, "%02x", 0);
 
-    sprintf (buffer + 24, "%04x", packetLength);
+	sprintf(buffer + 24, "%04x", packetLength);
 
+	// padding
+	sprintf(buffer + 28, "%04x", 0);
+	// padding
+	memcpy(buffer + 32, dataHex, packetLength);
 
-    // padding
-    sprintf (buffer + 28, "%04x", 0);
-    // padding
-    memcpy (buffer + 32, dataHex, packetLength);
+	uint32_t crc = CRC::Calculate(buffer, 30 + packetLength, CRC::CRC_32());
 
-    uint32_t crc
-        = CRC::Calculate (buffer, 30 + packetLength, CRC::CRC_32 ());
-
-    packet->packet->crc = crc;
-    // padding
-    sprintf (buffer + 30 + packetLength,
-             "%08x", // + 31 to skip the null terminator
-             packet->packet->crc);
-    return buffer;
+	packet->packet->crc = crc;
+	// padding
+	sprintf(buffer + 30 + packetLength,
+		"%08x", // + 31 to skip the null terminator
+		packet->packet->crc);
+	return buffer;
 }
--- src/src_tcp.h
+++ src/src_tcp.h
@@ -10,12 +10,11 @@
 /**
  * @brief SolarTcpPacket struct
  */
-struct SolarTcpPacket
-{
-    uint8_t type;
-    uint16_t length;
-    uint32_t crc;
-    char data[];
+struct SolarTcpPacket {
+	uint8_t type;
+	uint16_t length;
+	uint32_t crc;
+	char data[];
 };
 
 extern uint16_t count;
@@ -23,32 +22,26 @@ extern uint16_t count;
 /**
  * @brief SolarTcpPacketHeader struct
  */
-struct SolarTcpPacketHeader
-{
-    uint32_t timestamp = time (nullptr);
-    uint16_t sequence = count++;
-    uint16_t sender = 0;
-    uint16_t receiver = 0;
+struct SolarTcpPacketHeader {
+	uint32_t timestamp = time(nullptr);
+	uint16_t sequence = count++;
+	uint16_t sender = 0;
+	uint16_t receiver = 0;
 };
 
 /**
  * @brief SolarTcpPacket struct
  */
-struct SolarTcp
-{
-    SolarTcpPacketHeader *header;
-    SolarTcpPacket *packet;
+struct SolarTcp {
+	SolarTcpPacketHeader *header;
+	SolarTcpPacket *packet;
 };
 
 /**
  * @brief Server and Client pseudo addresses
  * @note used to identify the sender and receiver
  */
-enum DEVICES
-{
-    SERVER = 0x1B39,
-    CLIENT = 0x25E0
-};
+enum DEVICES { SERVER = 0x1B39, CLIENT = 0x25E0 };
 
 /**
  * @brief Packet types
@@ -60,14 +53,13 @@ enum DEVICES
  * @note 0x04 - Synchronization packet
  * @note 0x05 - Error packet
  */
-enum PACKET_TYPE
-{
-    INFO = 0x00,
-    DATA = 0x01,
-    ACK = 0x02,
-    RSP = 0x03,
-    SYN = 0x04,
-    ERR = 0x05
+enum PACKET_TYPE {
+	INFO = 0x00,
+	DATA = 0x01,
+	ACK = 0x02,
+	RSP = 0x03,
+	SYN = 0x04,
+	ERR = 0x05
 };
 
 /**
@@ -78,22 +70,22 @@ enum PACKET_TYPE
  * @return SolarTcpPacket*
  */
 
-SolarTcpPacket *createPacket (uint8_t length, char *data,
-                              PACKET_TYPE packetType);
+SolarTcpPacket *createPacket(uint8_t length, char *data,
+			     PACKET_TYPE packetType);
 
 /**
  * @brief Create a SolarTcpPacketHeader struct
  * @param devices One of the following: SERVER, CLIENT
  * @return
  */
-SolarTcpPacketHeader *createPacketHeader (DEVICES devices);
+SolarTcpPacketHeader *createPacketHeader(DEVICES devices);
 
 /**
  * @brief Convert an ASCII string to a hex string
  * @param ascii char*, a char array of ASCII characters
  * @param output char*, a char array to store the output hex string
  */
-void string2hexString (char *input, char *output);
+void string2hexString(char *input, char *output);
 
 /**
  * @brief Create a SolarTcp struct
@@ -101,14 +93,13 @@ void string2hexString (char *input, char *output);
  * @param packet SolarTcpPacket*
  * @return SolarTcp*
  */
-SolarTcp *createSolarTcp (SolarTcpPacketHeader *header,
-                          SolarTcpPacket *packet);
+SolarTcp *createSolarTcp(SolarTcpPacketHeader *header, SolarTcpPacket *packet);
 
 /**
  * @brief Convert a SolarTcp struct to raw data
  * @param packet
  * @return char* Raw data to be sent
  */
-char *SolarTcpRaw (SolarTcp *packet);
+char *SolarTcpRaw(SolarTcp *packet);
 
 #endif // SOLAR_TCP_SRC_TCP_H

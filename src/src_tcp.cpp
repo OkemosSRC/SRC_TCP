0d37f43181c1f980ae4301cdcbb16e8094c6d255
MatchaOnMuffins <103859142+matchaonmuffins@users.noreply.github.com>
2022-11-15 03:42:24 -0500

    Add raw data parsing
    Signed-off-by: MatchaOnMuffins <103859142+MatchaOnMuffins@users.noreply.github.com>

--- src/src_tcp.cpp
+++ src/src_tcp.cpp
@@ -7,26 +7,45 @@ SolarTcpPacket *createPacket(uint8_t length, char *data, PACKET_TYPE packetType)
 	if (packet == nullptr) {
 		perror("malloc");
 	}
-	// using malloc instead of new because we need to use free later
 	packet->type = packetType;
 	packet->length = length;
 	memcpy(packet->data, data, length);
 	return packet;
 }
 
+void changeCase(char *str, CASE c)
+{
+	int i = 0;
+	while (str[i] != '\0') {
+		if (c == LOWER) {
+			str[i] = (char)tolower(str[i]);
+		} else {
+			str[i] = (char)toupper(str[i]);
+		}
+		i++;
+	}
+}
+
 void string2hexString(char *input, char *output)
 {
-	int loop;
-	int i;
-
-	i = 0;
-	loop = 0;
-
+	int loop = 0;
+	int i = 0;
 	while (input[loop] != '\0') {
 		sprintf((char *)(output + i), "%02X", input[loop]);
 		loop += 1;
 		i += 2;
 	}
+}
+
+std::string hexString2string(const std::string &hex_input)
+{
+	std::string ascii_out;
+	for (size_t i = 0; i < hex_input.length(); i += 2) {
+		std::string part = hex_input.substr(i, 2);
+		char ch = (char)stoul(part, nullptr, 16);
+		ascii_out += ch;
+	}
+	return ascii_out;
 }
 
 SolarTcpPacketHeader *createPacketHeader(DEVICES devices)
@@ -54,7 +73,6 @@ SolarTcp *createSolarTcp(SolarTcpPacketHeader *header, SolarTcpPacket *packet)
 
 char *SolarTcpRaw(SolarTcp *packet)
 {
-	// convert char * to hex string
 	char *dataHex = (char *)malloc(packet->packet->length * 2);
 	string2hexString(packet->packet->data, dataHex);
 	int packetLength = packet->packet->length * 2;
@@ -64,22 +82,101 @@ char *SolarTcpRaw(SolarTcp *packet)
 	sprintf(buffer + 12, "%04x", packet->header->sender);
 	sprintf(buffer + 16, "%04x", packet->header->receiver);
 	sprintf(buffer + 20, "%02x", packet->packet->type);
-	// padding
 	sprintf(buffer + 22, "%02x", 0);
-
 	sprintf(buffer + 24, "%04x", packetLength);
-
-	// padding
 	sprintf(buffer + 28, "%04x", 0);
-	// padding
 	memcpy(buffer + 32, dataHex, packetLength);
-
+	changeCase(buffer, LOWER);
 	uint32_t crc = CRC::Calculate(buffer, 30 + packetLength, CRC::CRC_32());
-
 	packet->packet->crc = crc;
-	// padding
 	sprintf(buffer + 30 + packetLength,
-		"%08x", // + 31 to skip the null terminator
+		"%08x", // + 30 to skip the null terminator
 		packet->packet->crc);
+	changeCase(buffer, LOWER);
+	free(dataHex);
 	return buffer;
+}
+
+SolarRcvTcp *RawToSolarTcp(char *raw)
+{
+	char *lengthOfRaw = (char *)malloc(4);
+	memcpy(lengthOfRaw, raw + 24, 4);
+	lengthOfRaw[4] = '\0';
+	int packetLength = (int)strtol(lengthOfRaw, nullptr, 16);
+#if VERBOSE
+	printf("PacketLength: %d\n", packetLength);
+#endif
+	int rawLength = packetLength + 30;
+#if VERBOSE
+	printf("RawLength: %d\n", rawLength);
+#endif
+	uint32_t crcCheck = CRC::Calculate(raw, rawLength, CRC::CRC_32());
+	char *crcCheckString = (char *)malloc(8);
+	sprintf(crcCheckString, "%08x", crcCheck);
+	changeCase(crcCheckString, LOWER);
+#if VERBOSE
+	printf("CrcCheckString: %s\n", crcCheckString);
+#endif
+	char *crcString = (char *)malloc(8);
+	memcpy(crcString, raw + rawLength, 8);
+	crcString[8] = '\0';
+	changeCase(crcString, LOWER);
+#if VERBOSE
+	printf("CrcString: %s\n", crcString);
+#endif
+	if (strcmp(crcString, crcCheckString) != 0) {
+		printf("CRC check failed\n");
+		return nullptr;
+	} else {
+#if VERBOSE
+		printf("CRC check passed\n\n");
+#endif
+		// SOLAR TCP PACKET HEADER
+		auto *header = new SolarTcpPacketRcvHeader{};
+		char *timestamp = (char *)malloc(8);
+		memcpy(timestamp, raw, 8);
+		timestamp[8] = '\0';
+		header->timestamp = strtol(timestamp, nullptr, 16);
+
+		char *sequence = (char *)malloc(4);
+		memcpy(sequence, raw + 8, 4);
+		sequence[4] = '\0';
+		header->sequence = strtol(sequence, nullptr, 16);
+
+		char *sender = (char *)malloc(4);
+		memcpy(sender, raw + 12, 4);
+		sender[4] = '\0';
+		header->sender = strtol(sender, nullptr, 16);
+
+		char *receiver = (char *)malloc(4);
+		memcpy(receiver, raw + 16, 4);
+		receiver[4] = '\0';
+		header->receiver = strtol(receiver, nullptr, 16);
+
+		// SOLAR TCP PACKET
+		auto *packet = new SolarTcpPacket{};
+		char *type = (char *)malloc(2);
+		memcpy(type, raw + 20, 2);
+		type[2] = '\0';
+		packet->type = strtol(type, nullptr, 16);
+
+		char *length = (char *)malloc(4);
+		memcpy(length, raw + 24, 4);
+		length[4] = '\0';
+		packet->length = strtol(length, nullptr, 16) / 2;
+
+		char *data = (char *)malloc(packetLength);
+		memcpy(data, raw + 32, packetLength);
+		data[packetLength - 2] = '\0';
+		std::string thing = hexString2string(data);
+		memcpy(packet->data, (char *)thing.c_str(), packet->length);
+
+		char *crc = (char *)malloc(8);
+		memcpy(crc, raw + rawLength, 8);
+		crc[8] = '\0';
+		packet->crc = strtol(crc, nullptr, 16);
+
+		auto *solarTcp = new SolarRcvTcp{ header, packet };
+		return solarTcp;
+	}
 }
--- src/src_tcp.h
+++ src/src_tcp.h
@@ -1,5 +1,6 @@
 #ifndef SOLAR_TCP_SRC_TCP_H
 #define SOLAR_TCP_SRC_TCP_H
+#define VERBOSE 1
 
 #include "crc/CRC.h"
 #include <cstdint>
@@ -7,6 +8,9 @@
 #include <iomanip>
 #include <iostream>
 #include <string>
+
+extern uint16_t count;
+
 /**
  * @brief SolarTcpPacket struct
  */
@@ -16,8 +20,6 @@ struct SolarTcpPacket {
 	uint32_t crc;
 	char data[];
 };
-
-extern uint16_t count;
 
 /**
  * @brief SolarTcpPacketHeader struct
@@ -34,6 +36,24 @@ struct SolarTcpPacketHeader {
  */
 struct SolarTcp {
 	SolarTcpPacketHeader *header;
+	SolarTcpPacket *packet;
+};
+
+/**
+ * @brief SolarTcpPacketRcvHeader struct
+ */
+struct SolarTcpPacketRcvHeader {
+	uint32_t timestamp = 0;
+	uint16_t sequence = 0;
+	uint16_t sender = 0;
+	uint16_t receiver = 0;
+};
+
+/**
+ * @brief SolarTcpRcvPacket struct
+ */
+struct SolarRcvTcp {
+	SolarTcpPacketRcvHeader *header;
 	SolarTcpPacket *packet;
 };
 
@@ -63,29 +83,27 @@ enum PACKET_TYPE {
 };
 
 /**
+ * @brief enum for upper and lower case
+ * @note used in the changeCase function
+ */
+enum CASE { LOWER, UPPER };
+
+/**
  * @brief Create a SolarTcpPacket struct
  * @param length uint8_t, length of data
  * @param data char*, a char array
  * @param packetType One of the following: INFO, DATA, ACK, RSP, SYN, ERR
  * @return SolarTcpPacket*
  */
-
 SolarTcpPacket *createPacket(uint8_t length, char *data,
 			     PACKET_TYPE packetType);
 
 /**
  * @brief Create a SolarTcpPacketHeader struct
  * @param devices One of the following: SERVER, CLIENT
- * @return
+ * @return SolarTcpPacketHeader*
  */
 SolarTcpPacketHeader *createPacketHeader(DEVICES devices);
-
-/**
- * @brief Convert an ASCII string to a hex string
- * @param ascii char*, a char array of ASCII characters
- * @param output char*, a char array to store the output hex string
- */
-void string2hexString(char *input, char *output);
 
 /**
  * @brief Create a SolarTcp struct
@@ -97,9 +115,37 @@ SolarTcp *createSolarTcp(SolarTcpPacketHeader *header, SolarTcpPacket *packet);
 
 /**
  * @brief Convert a SolarTcp struct to raw data
- * @param packet
+ * @param packet SolarTcp*
  * @return char* Raw data to be sent
  */
 char *SolarTcpRaw(SolarTcp *packet);
+
+/**
+ * @brief Convert a raw data to a SolarTcp struct
+ * @param raw char*, a char array of raw data
+ * @return SolarTcp*
+ */
+SolarRcvTcp *RawToSolarTcp(char *raw);
+
+/**
+ * @brief Convert an ASCII string to a hex string
+ * @param ascii char*, a char array of ASCII characters
+ * @param output char*, a char array to store the output hex string
+ */
+void string2hexString(char *input, char *output);
+
+/**
+ * @brief Convert a hex string to an ASCII string
+ * @param hex_input std::string, a string of hex characters
+ * @return std::string, a string of ASCII characters
+ */
+std::string hexString2string(const std::string &hex_input);
+
+/**
+ * @brief Convert a char * from lower case to upper case or vice versa
+ * @param str
+ * @param c One of the following: LOWER, UPPER
+ */
+void changeCase(char *str, CASE c);
 
 #endif // SOLAR_TCP_SRC_TCP_H
